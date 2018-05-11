0167c8882f221fd852993cf5418dc3f986f0a442
Fabricio Oliveira <netwolf.fabricio@gmail.com>
2018-05-11 11:28:08 -0300

    Initial Commit

---
+++ ShareImageInstagram/ShareImageInstagram.h
@@ -0,0 +1,19 @@
+//
+//  ShareImageInstagram.h
+//  ShareImageInstagram
+//
+//  Created by Fabricio Oliveira on 5/11/18.
+//  Copyright © 2018 Fabricio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShareImageInstagram.
+FOUNDATION_EXPORT double ShareImageInstagramVersionNumber;
+
+//! Project version string for ShareImageInstagram.
+FOUNDATION_EXPORT const unsigned char ShareImageInstagramVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShareImageInstagram/PublicHeader.h>
+
+

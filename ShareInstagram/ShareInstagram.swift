2fe322a626c898077fbe3338c65f25a81a797397
Fabricio Oliveira <netwolf.fabricio@gmail.com>
2020-05-23 00:09:11 +0200

    Swift 5 Update

--- ShareInstagram/ShareInstagram.swift
+++ ShareInstagram/ShareInstagram.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 import Photos
 
-public protocol ShareStoriesDelegate {
+public protocol ShareStoriesDelegate: class {
     func error(message: String)
     func success()
 }
@@ -18,19 +18,15 @@ public protocol ShareStoriesDelegate {
 open class ShareImageInstagram {
     
     private let instagramURL = URL(string: "instagram://app")
-    private let instagramStoriesURL = URL(string: "instagram-stories://share")
+    private let storiesURL = URL(string: "instagram-stories://share")
     
-    var delegate: ShareStoriesDelegate?
+    weak var delegate: ShareStoriesDelegate?
     
-    public init() {
-        
-    }
-    
-    public func postToInstagramFeed(image: UIImage, caption: String, bounds: CGRect, view: UIView) {
+    public func postToFeed(image: UIImage, caption: String, bounds: CGRect, view: UIView) {
         
         PHPhotoLibrary.shared().performChanges({
             PHAssetChangeRequest.creationRequestForAsset(from: image)
-        }, completionHandler: { success, error in
+        }, completionHandler: { [weak self] success, error in
             if success {
                 let fetchOptions = PHFetchOptions()
                 fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
@@ -39,50 +35,46 @@ open class ShareImageInstagram {
                     let localIdentifier = lastAsset.localIdentifier
                     let urlFeed = "instagram://library?LocalIdentifier=" + localIdentifier
                     guard let url = URL(string: urlFeed) else {
-                        self.delegate?.error(message: "Could not open url")
+                        self?.delegate?.error(message: "Could not open url")
                         return
                     }
                     DispatchQueue.main.async {
                         if UIApplication.shared.canOpenURL(url) {
                             if #available(iOS 10.0, *) {
                                 UIApplication.shared.open(url, options: [:], completionHandler: { (success) in
-                                    self.delegate?.success()
+                                    self?.delegate?.success()
                                 })
                             } else {
                                 UIApplication.shared.openURL(url)
-                                self.delegate?.success()
+                                self?.delegate?.success()
                                 
                             }
                         } else {
-                            self.delegate?.error(message: "Instagram not found")
+                            self?.delegate?.error(message: "Instagram not found")
                         }
                     }
                 }
             } else if let error = error {
-                self.delegate?.error(message: error.localizedDescription)
+                self?.delegate?.error(message: error.localizedDescription)
             }
             else {
-                self.delegate?.error(message: "Could not save the photo")
+                self?.delegate?.error(message: "Could not save the photo")
             }
         })
     }
     
-    public func postToInstagramStories(data: NSData, image: UIImage, backgroundTopColorHex: String, backgroundBottomColorHex: String, deepLink: String) {
+    public func postToStories(data: NSData, image: UIImage, backgroundTopColorHex: String, backgroundBottomColorHex: String, deepLink: String) {
         
-        DispatchQueue.main.async {
-            
-            guard let url = self.instagramURL else {
-                self.delegate?.error(message: "URL not valid")
+        DispatchQueue.main.async { [weak self] in
+            guard let url = self?.instagramURL else {
+                self?.delegate?.error(message: "URL not valid")
                 return
             }
-            
             if UIApplication.shared.canOpenURL(url) {
-                
-                guard let urlScheme = self.instagramStoriesURL else {
-                    self.delegate?.error(message: "URL not valid")
+                guard let urlScheme = self?.storiesURL else {
+                    self?.delegate?.error(message: "URL not valid")
                     return
                 }
-                
                 let pasteboardItems = ["com.instagram.sharedSticker.stickerImage": image,
                                        "com.instagram.sharedSticker.backgroundTopColor" : backgroundTopColorHex,
                                        "com.instagram.sharedSticker.backgroundBottomColor" : backgroundBottomColorHex,
@@ -90,7 +82,7 @@ open class ShareImageInstagram {
                                        "com.instagram.sharedSticker.contentURL": deepLink] as [String : Any]
                 
                 if #available(iOS 10.0, *) {
-                    let pasteboardOptions = [UIPasteboardOption.expirationDate : NSDate().addingTimeInterval(60 * 5)]
+                    let pasteboardOptions = [UIPasteboard.OptionsKey.expirationDate : NSDate().addingTimeInterval(60 * 5)]
                     UIPasteboard.general.setItems([pasteboardItems], options: pasteboardOptions)
                     
                 } else {
@@ -98,58 +90,52 @@ open class ShareImageInstagram {
                 }
                 if #available(iOS 10.0, *) {
                     UIApplication.shared.open(urlScheme, options: [:], completionHandler: { (success) in
-                        self.delegate?.success()
+                        self?.delegate?.success()
                     })
                 } else {
                     UIApplication.shared.openURL(urlScheme)
-                    self.delegate?.success()
+                    self?.delegate?.success()
                 }
                 
             } else {
-                self.delegate?.error(message: "Could not open instagram URL. Check if you have instagram installed and you configured your LSApplicationQueriesSchemes to enable instagram's url")
+                self?.delegate?.error(message: "Could not open instagram URL. Check if you have instagram installed and you configured your LSApplicationQueriesSchemes to enable instagram's url")
             }
         }
+        
     }
     
-    public func postToInstagramStories(image: UIImage, backgroundTopColorHex: String, backgroundBottomColorHex: String, deepLink: String) {
+    public func postToStories(image: UIImage, backgroundTopColorHex: String, backgroundBottomColorHex: String, deepLink: String) {
         
-        DispatchQueue.main.async {
-            
-            guard let url = self.instagramURL else {
-                self.delegate?.error(message: "URL not valid")
+        DispatchQueue.main.async { [weak self] in
+            guard let url = self?.instagramURL else {
+                self?.delegate?.error(message: "URL not valid")
                 return
             }
-            
             if UIApplication.shared.canOpenURL(url) {
-                
-                guard let urlScheme = self.instagramStoriesURL else {
-                    self.delegate?.error(message: "URL not valid")
+                guard let urlScheme = self?.storiesURL else {
+                    self?.delegate?.error(message: "URL not valid")
                     return
                 }
-                
                 let pasteboardItems = ["com.instagram.sharedSticker.stickerImage": image,
                                        "com.instagram.sharedSticker.backgroundTopColor" : backgroundTopColorHex,
                                        "com.instagram.sharedSticker.backgroundBottomColor" : backgroundBottomColorHex,
                                        "com.instagram.sharedSticker.contentURL": deepLink] as [String : Any]
-                
                 if #available(iOS 10.0, *) {
-                    let pasteboardOptions = [UIPasteboardOption.expirationDate : NSDate().addingTimeInterval(60 * 5)]
+                    let pasteboardOptions = [UIPasteboard.OptionsKey.expirationDate : NSDate().addingTimeInterval(60 * 5)]
                     UIPasteboard.general.setItems([pasteboardItems], options: pasteboardOptions)
-                    
                 } else {
                     UIPasteboard.general.items = [pasteboardItems]
                 }
                 if #available(iOS 10.0, *) {
                     UIApplication.shared.open(urlScheme, options: [:], completionHandler: { (success) in
-                        self.delegate?.success()
+                        self?.delegate?.success()
                     })
                 } else {
                     UIApplication.shared.openURL(urlScheme)
-                    self.delegate?.success()
+                    self?.delegate?.success()
                 }
-                
             } else {
-                self.delegate?.error(message: "Could not open instagram URL. Check if you have instagram installed and you configured your LSApplicationQueriesSchemes to enable instagram's url")
+                self?.delegate?.error(message: "Could not open instagram URL. Check if you have instagram installed and you configured your LSApplicationQueriesSchemes to enable instagram's url")
             }
         }
     }
